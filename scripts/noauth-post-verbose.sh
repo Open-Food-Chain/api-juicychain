cb90eb49fa531287a90095bc239168c8ae261009
MyloHDEX <mylo@hybriddex.com>
2020-10-01 19:50:53 +0000

    scripts update

--- scripts/noauth-post-verbose.sh
+++ scripts/noauth-post-verbose.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-API_HOST="http://172.29.0.5:8999/"
-#API_HOST="http://juicychain-api.thenewfork.staging.do.unchain.io/"
+#API_HOST="http://172.29.0.5:8999/"
+API_HOST="http://juicychain-api.thenewfork.staging.do.unchain.io/"
 API_VER="api/v1/"
 
 for i in {1..2}
